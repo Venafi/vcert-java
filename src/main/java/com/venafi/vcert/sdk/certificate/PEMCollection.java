4fa4d6bcdc17fd989cb2a5f54172962a27389294
Hongli Lai <h.lai@fullstaq.com>
2020-11-07 00:13:58 +0100

    Support outputting in PKCS12, DER and JKS formats (#27)
    * Support creating DER files
    * Support creating PKCS12 files
    * Support creating JKS files

--- src/main/java/com/venafi/vcert/sdk/certificate/PEMCollection.java
+++ src/main/java/com/venafi/vcert/sdk/certificate/PEMCollection.java
@@ -4,21 +4,51 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.StringReader;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.asn1.ASN1Encoding;
+import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
 import org.bouncycastle.openssl.PEMEncryptor;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.openssl.jcajce.JcePEMEncryptorBuilder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.OutputEncryptor;
+import org.bouncycastle.pkcs.PKCS12PfxPdu;
+import org.bouncycastle.pkcs.PKCS12PfxPduBuilder;
+import org.bouncycastle.pkcs.PKCS12SafeBag;
+import org.bouncycastle.pkcs.PKCSException;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS12SafeBagBuilder;
+import org.bouncycastle.pkcs.jcajce.JcePKCS12MacCalculatorBuilder;
+import org.bouncycastle.pkcs.jcajce.JcePKCSPBEOutputEncryptorBuilder;
 import org.bouncycastle.util.io.pem.PemObject;
 import org.bouncycastle.util.io.pem.PemWriter;
 import lombok.Data;
@@ -33,15 +63,21 @@ public class PEMCollection {
   // resulting in an "illegal key size" exception.
   // https://deveshsharmablogs.wordpress.com/2012/10/09/fixing-java-security-invalidkeyexception-illegal-key-size-exception/
   public static final String BOUNCY_CASTLE_ENCRYPTION_ALGORITHM = "AES-128-CBC";
+  public static final String CIPHER_TRANSFORMATION = "AES/CBC/PKCS5Padding";
+  public static final String SECRET_KEY_FACTORY_ALGORITHM = "PBKDF-OpenSSL";
+  public static final String SECRET_KEY_ALGORITHM = "AES";
+  // We don't use AES with 256-bit key: see comment for
+  // BOUNCY_CASTLE_ENCRYPTION_ALGORITHM.
+  public static final int SECRET_KEY_LENGTH_BITS = 128;
 
-  private Certificate certificate;
+  private X509Certificate certificate;
   private PrivateKey privateKey;
   private String privateKeyPassword;
-  private List<Certificate> chain = new ArrayList<>();
+  private List<X509Certificate> chain = new ArrayList<>();
 
   public static PEMCollection fromResponse(String body, ChainOption chainOption,
       PrivateKey privateKey, String privateKeyPassword) throws VCertException {
-    List<Certificate> chain = new ArrayList<>();
+    List<X509Certificate> chain = new ArrayList<>();
 
     PEMParser pemParser = new PEMParser(new StringReader(body));
     JcaX509CertificateConverter certificateConverter = new JcaX509CertificateConverter();
@@ -52,7 +88,7 @@ public class PEMCollection {
         if (object instanceof X509CertificateHolder) {
           Certificate certificate =
               certificateConverter.getCertificate((X509CertificateHolder) object);
-          chain.add(certificate);
+          chain.add((X509Certificate) certificate);
         } else if (object instanceof PEMKeyPair) {
           privateKey = keyConverter.getPrivateKey(((PEMKeyPair) object).getPrivateKeyInfo());
         }
@@ -148,5 +184,153 @@ public class PEMCollection {
       }
     }
     return pem.toString();
+  }
+
+  public byte[] derCertificate() {
+    if (Objects.isNull(this.certificate)) {
+      return null;
+    }
+
+    try {
+      return this.certificate.getEncoded();
+    } catch (CertificateEncodingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public RawPrivateKey derPrivateKey() {
+    if (Objects.isNull(this.privateKey)) {
+      return null;
+    }
+
+    try {
+      RawPrivateKey result = new RawPrivateKey();
+
+      if (KeyType.from(this.privateKey.getAlgorithm()) == KeyType.RSA) {
+        PrivateKeyInfo pkInfo = PrivateKeyInfo.getInstance(this.privateKey.getEncoded());
+        ASN1Primitive privateKeyPKCS1ASN1 = pkInfo.parsePrivateKey().toASN1Primitive();
+        result.data = privateKeyPKCS1ASN1.getEncoded();
+      } else {
+        result.data = this.privateKey.getEncoded();
+      }
+
+      if (privateKeyPassword == null) {
+        return result;
+      } else {
+        result.iv = new byte[SECRET_KEY_LENGTH_BITS / 8];
+        new SecureRandom().nextBytes(result.iv);
+        SecretKeySpec secretKey = passwordToCipherSecretKey(privateKeyPassword.toCharArray(), result.iv);
+        Cipher c = Cipher.getInstance(CIPHER_TRANSFORMATION);
+        c.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(result.iv));
+        result.data = c.doFinal(result.data);
+        return result;
+      }
+    } catch (IOException | GeneralSecurityException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public List<byte[]> derCertificateChain() {
+    if (Objects.isNull(this.chain)) {
+      return null;
+    }
+
+    ArrayList<byte[]> result = new ArrayList<>();
+    result.ensureCapacity(this.chain.size());
+    for (Certificate cert : this.chain) {
+      try {
+        result.add(cert.getEncoded());
+      } catch (CertificateEncodingException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return result;
+  }
+
+  public byte[] toPkcs12(String password) throws PKCSException {
+    try {
+      SubjectKeyIdentifier pubKeyId = new JcaX509ExtensionUtils()
+        .createSubjectKeyIdentifier(certificate.getPublicKey());
+
+      OutputEncryptor encOut = new JcePKCSPBEOutputEncryptorBuilder(NISTObjectIdentifiers.id_aes128_CBC)
+        .setProvider("BC")
+        .build(password.toCharArray());
+      ArrayList<PKCS12SafeBag> safeBags = new ArrayList<>();
+
+      safeBags.ensureCapacity(chain.size() + 2);
+      safeBags.add(new JcaPKCS12SafeBagBuilder((X509Certificate) certificate)
+        .addBagAttribute(PKCS12SafeBag.localKeyIdAttribute, pubKeyId)
+        .build());
+      for (Certificate intermediateCert: chain) {
+        safeBags.add(
+          new JcaPKCS12SafeBagBuilder((X509Certificate) intermediateCert)
+            .build());
+      }
+      safeBags.add(new JcaPKCS12SafeBagBuilder(privateKey, encOut)
+        .addBagAttribute(PKCS12SafeBag.localKeyIdAttribute, pubKeyId)
+        .build());
+
+
+      PKCS12PfxPduBuilder builder = new PKCS12PfxPduBuilder();
+      builder.addEncryptedData(
+        new JcePKCSPBEOutputEncryptorBuilder(PKCSObjectIdentifiers.pbeWithSHAAnd128BitRC2_CBC)
+          .setProvider("BC")
+          .build(password.toCharArray()),
+        safeBags.toArray(new PKCS12SafeBag[]{}));
+
+      PKCS12PfxPdu pfx = builder.build(
+        new JcePKCS12MacCalculatorBuilder(NISTObjectIdentifiers.id_sha256),
+        password.toCharArray());
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      out.write(pfx.getEncoded(ASN1Encoding.DL));
+      out.close();
+      return out.toByteArray();
+    } catch (IOException | NoSuchAlgorithmException | OperatorCreationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public byte[] toJks(String password) throws KeyStoreException, CertificateException {
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    KeyStore store;
+
+    try {
+      store = KeyStore.getInstance(KeyStore.getDefaultType());
+      store.load(null, password.toCharArray());
+    } catch (KeyStoreException | NoSuchAlgorithmException | IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    List<X509Certificate> chain = new ArrayList<>();
+    chain.add(this.certificate);
+    chain.addAll(this.chain);
+    store.setKeyEntry("private-key", privateKey, password.toCharArray(),
+      chain.toArray(new X509Certificate[] {}));
+
+    try {
+      store.store(output, password.toCharArray());
+    } catch (NoSuchAlgorithmException | IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return output.toByteArray();
+  }
+
+  public static SecretKeySpec passwordToCipherSecretKey(char[] password, byte[] iv)
+      throws NoSuchAlgorithmException, InvalidKeySpecException {
+    PBEKeySpec spec = new PBEKeySpec(password, iv, 1, SECRET_KEY_LENGTH_BITS);
+    SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(SECRET_KEY_FACTORY_ALGORITHM);
+    byte[] key = keyFactory.generateSecret(spec).getEncoded();
+    return new SecretKeySpec(key, SECRET_KEY_ALGORITHM);
+  }
+
+  @Data
+  public static class RawPrivateKey {
+    private byte[] iv;
+    private byte[] data;
+
+    public boolean isEncrypted() {
+      return iv != null;
+    }
   }
 }
--- src/test/java/com/venafi/vcert/sdk/certificate/PEMCollectionTest.java
+++ src/test/java/com/venafi/vcert/sdk/certificate/PEMCollectionTest.java
@@ -1,18 +1,44 @@
 package com.venafi.vcert.sdk.certificate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.asn1.pkcs.ContentInfo;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.bc.BcDefaultDigestProvider;
+import org.bouncycastle.pkcs.PKCS12PfxPdu;
+import org.bouncycastle.pkcs.PKCSException;
+import org.bouncycastle.pkcs.bc.BcPKCS12MacCalculatorBuilderProvider;
 import org.junit.jupiter.api.Test;
 import com.venafi.vcert.sdk.VCertException;
 
 class PEMCollectionTest {
   private static final String KEY_PASSWORD = "my secret";
+  private static final String PKCS12_PASSWORD = "abcd";
 
   static {
     Security.addProvider(new BouncyCastleProvider());
@@ -70,5 +96,114 @@ class PEMCollectionTest {
     String pemPrivateKey = pemCollection2.pemPrivateKey();
     assertThat(pemPrivateKey).contains("BEGIN EC PRIVATE KEY");
     assertThat(pemPrivateKey).contains("ENCRYPTED");
+  }
+
+  @Test
+  void derCertificate() throws VCertException, IOException, CertificateException {
+    String body = readResourceAsString("certificates/certWithKey.pem");
+    PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionIgnore, null, null);
+
+    byte[] derData = pemCollection.derCertificate();
+    CertificateFactory cf = CertificateFactory.getInstance("X.509");
+    X509Certificate cert = (X509Certificate) cf.generateCertificate(new ByteArrayInputStream(derData));
+    assertThat(cert.getSubjectX500Principal().getName()).isEqualTo("CN=test@test.com");
+  }
+
+  @Test
+  void derCertificateChain() throws VCertException, IOException, CertificateException {
+    String body = readResourceAsString("certificates/certWithChainAndKey.pem");
+    PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionRootLast, null, null);
+
+    List<byte[]> derChain = pemCollection.derCertificateChain();
+    assertThat(derChain.size()).isEqualTo(2);
+
+    CertificateFactory cf = CertificateFactory.getInstance("X.509");
+    X509Certificate cert = (X509Certificate) cf.generateCertificate(new ByteArrayInputStream(derChain.get(0)));
+    assertThat(cert.getSubjectX500Principal().getName()).isEqualTo("CN=Dedicated - Venafi Cloud Built-In Intermediate CA - G1,OU=Built-in,O=Venafi\\, Inc.,C=US");
+  }
+
+  @Test
+  void derPrivateKeyWithoutPassword() throws VCertException, IOException, GeneralSecurityException {
+    String body = readResourceAsString("certificates/certWithKey.pem");
+    PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionIgnore, null, null);
+
+    PEMCollection.RawPrivateKey privKey = pemCollection.derPrivateKey();
+    assertThat(privKey.isEncrypted()).isFalse();
+    assertThatCode(() -> {
+      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privKey.data());
+      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      keyFactory.generatePrivate(keySpec);
+    }).doesNotThrowAnyException();
+  }
+
+  @Test
+  void derPrivateKeyWithPassword() throws VCertException, IOException, GeneralSecurityException {
+    String body = readResourceAsString("certificates/certWithKey.pem");
+    PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionIgnore, null,
+      KEY_PASSWORD);
+
+    PEMCollection.RawPrivateKey privKey = pemCollection.derPrivateKey();
+    assertThat(privKey.isEncrypted()).isTrue();
+    assertThatExceptionOfType(InvalidKeySpecException.class).isThrownBy(() -> {
+      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privKey.data());
+      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      keyFactory.generatePrivate(keySpec);
+    });
+
+    byte[] decryptedPrivKey;
+    SecretKeySpec secretKey = PEMCollection.passwordToCipherSecretKey(
+      KEY_PASSWORD.toCharArray(), privKey.iv());
+    Cipher c = Cipher.getInstance(PEMCollection.CIPHER_TRANSFORMATION);
+    c.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(privKey.iv()));
+    decryptedPrivKey = c.doFinal(privKey.data());
+
+    assertThatCode(() -> {
+      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decryptedPrivKey);
+      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      keyFactory.generatePrivate(keySpec);
+    }).doesNotThrowAnyException();
+  }
+
+  @Test
+  void toPkcs12() throws VCertException, IOException, GeneralSecurityException, PKCSException {
+    String body = readResourceAsString("certificates/certWithKey.pem");
+    PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionIgnore, null, null);
+
+    byte[] pkcs12Data = pemCollection.toPkcs12(PKCS12_PASSWORD);
+
+    PKCS12PfxPdu pfx = new PKCS12PfxPdu(pkcs12Data);
+    assertThat(
+      pfx.isMacValid(
+        new BcPKCS12MacCalculatorBuilderProvider(BcDefaultDigestProvider.INSTANCE),
+        PKCS12_PASSWORD.toCharArray()
+      )
+    ).isTrue();
+
+    ContentInfo[] infos = pfx.getContentInfos();
+    assertThat(infos.length).isEqualTo(1);
+    assertThat(infos[0].getContentType()).isEqualTo(PKCSObjectIdentifiers.encryptedData);
+  }
+
+  @Test
+  void toJks() throws VCertException, IOException, GeneralSecurityException {
+    String body = readResourceAsString("certificates/certWithChainAndKey.pem");
+    PEMCollection pemCollection = PEMCollection.fromResponse(body, ChainOption.ChainOptionRootLast, null, null);
+
+    byte[] jksData = pemCollection.toJks(KEY_PASSWORD);
+
+    KeyStore store = KeyStore.getInstance(KeyStore.getDefaultType());
+    store.load(new ByteArrayInputStream(jksData), KEY_PASSWORD.toCharArray());
+    ArrayList<String> aliases = Collections.list(store.aliases());
+    assertThat(aliases.size()).isEqualTo(1);
+    assertThat(aliases.get(0)).isEqualTo("private-key");
+
+    KeyStore.PrivateKeyEntry entry = (KeyStore.PrivateKeyEntry) store.getEntry("private-key",
+      new KeyStore.PasswordProtection(KEY_PASSWORD.toCharArray()));
+    assertThat(entry.getPrivateKey().getEncoded()).isEqualTo(
+      pemCollection.privateKey().getEncoded());
+    assertThat(entry.getCertificate().getEncoded()).isEqualTo(
+      pemCollection.certificate().getEncoded());
+    assertThat(entry.getCertificateChain().length).isEqualTo(
+      pemCollection.chain().size() + 1);
   }
 }
